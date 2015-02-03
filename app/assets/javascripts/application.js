344f3a9a4483a1282f813f50106859163277dde2
Zoe <doticus@gmail.com>
2015-02-03 17:35:19 -0500

    This should do it for API query

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,15 @@
 // = function handleAPILoaded() {
 //   $('#search-button').attr('disabled', false);
 // }
-// 
+//
 // // Search for a specified string.
 // function search() {
 //   var q = $('#query').val();
 //   var request = gapi.client.youtube.search.list({
-//     q: q,
-//     part: 'snippet'
-//   });
+//        part: 'snippet',
+//        q: "Cooking with Dog"
+//    });
+//
 //
 //   request.execute(function(response) {
 //     var str = JSON.stringify(response.result);
